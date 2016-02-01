e0cb01f6adb9df44bc2b9d95da2b325a12734d5c
Gleb Kozyrev <gleb@gkoz.com>
2016-02-01 22:16:47 +0200

    Make this a gtk-rs-lgpl-docs crate

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub const CAIRO_DOCS: &'static str = include_str!("../cairo/comments.cmts");
+pub const GDK_DOCS: &'static str = include_str!("../gdk/comments.cmts");
+pub const GLIB_DOCS: &'static str = include_str!("../glib/comments.cmts");
+pub const GTK_DOCS: &'static str = include_str!("../gtk/comments.cmts");
+pub const PANGO_DOCS: &'static str = include_str!("../pango/comments.cmts");
