76a1758189f36dca1059f28a6c1b3ffc6697b92d
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-02-19 23:33:08 +0100

    gtk4: add gdk4-x11 & gdk4-wayland

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,7 @@ extern crate stripper_lib;
 
 use std::io;
 use std::path::Path;
-use stripper_lib::{
-    loop_over_files,
-    parse_cmts,
-    regenerate_comments,
-    strip_comments,
-};
+use stripper_lib::{loop_over_files, parse_cmts, regenerate_comments, strip_comments};
 
 #[derive(Clone, Copy, Debug)]
 pub enum Library {
@@ -15,6 +10,8 @@ pub enum Library {
     Cairo,
     Gdk,
     Gdk4,
+    Gdk4X11,
+    Gdk4Wayland,
     GdkPixbuf,
     Gio,
     Glib,
@@ -34,6 +31,8 @@ fn docs(lib: Library) -> Option<&'static str> {
         Library::Cairo => Some(include_str!("../cairo/docs.md")),
         Library::Gdk => Some(include_str!("../gdk/docs.md")),
         Library::Gdk4 => Some(include_str!("../gdk4/docs.md")),
+        Library::Gdk4X11 => Some(include_str!("../gdk4-x11/docs.md")),
+        Library::Gdk4Wayland => Some(include_str!("../gdk4-wayland/docs.md")),
         Library::GdkPixbuf => Some(include_str!("../gdk-pixbuf/docs.md")),
         Library::Graphene => Some(include_str!("../graphene/docs.md")),
         Library::Gsk4 => Some(include_str!("../gsk4/docs.md")),
@@ -51,6 +50,8 @@ fn vendor_docs(lib: Library) -> Option<&'static str> {
         Library::Pango => Some(include_str!("../pango/vendor.md")),
         Library::Gdk => Some(include_str!("../gdk/vendor.md")),
         Library::Gdk4 => Some(include_str!("../gdk4/vendor.md")),
+        Library::Gdk4X11 => Some(include_str!("../gdk4-wayland/vendor.md")),
+        Library::Gdk4Wayland => Some(include_str!("../gdk4-wayland/vendor.md")),
         Library::GdkPixbuf => Some(include_str!("../gdk-pixbuf/vendor.md")),
         Library::Graphene => Some(include_str!("../graphene/vendor.md")),
         Library::Gsk4 => Some(include_str!("../gsk4/vendor.md")),
@@ -78,8 +79,12 @@ pub fn embed<P: AsRef<Path>>(library: Library, path: P, ignores: &[&str]) {
 
 fn do_embed(docs: &str, path: &Path, ignores: &[&str]) {
     let mut infos = parse_cmts(docs.lines(), true);
-    loop_over_files(path, &mut |w, s| regenerate_comments(w, s, &mut infos, true, true),
-        &ignores, false);
+    loop_over_files(
+        path,
+        &mut |w, s| regenerate_comments(w, s, &mut infos, true, true),
+        &ignores,
+        false,
+    );
 }
 
 /// Remove any doc comments.
@@ -88,6 +93,10 @@ fn do_embed(docs: &str, path: &Path, ignores: &[&str]) {
 ///
 /// `ignores` is the list of files to skip (relative to `path`).
 pub fn purge<P: AsRef<Path>>(path: P, ignores: &[&str]) {
-    loop_over_files(path.as_ref(), &mut |w, s| strip_comments(w, s, &mut io::sink(), true),
-        &ignores, false);
+    loop_over_files(
+        path.as_ref(),
+        &mut |w, s| strip_comments(w, s, &mut io::sink(), true),
+        &ignores,
+        false,
+    );
 }
